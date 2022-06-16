b8dc00a3ad939cf8708661eba2eeb2087dad2cb1
ZeroDawn0D <umang26112003@gmail.com>
2022-06-16 08:52:30 +0530

    removed warnings from R CMD check

--- R/data.R
+++ R/data.R
@@ -35,8 +35,8 @@
 #'  \item \strong{circle_x}: x-values for the \code{circle} dataset
 #'  \item \strong{circle_y}: y-values for the \code{circle} dataset
 #'  \item \strong{dino_x}: x-values for \code{dinosaur} dataset!
-#'    \item \strong{dino_y}: y-values for \code{dinosaur} dataset!
-#'    \item \strong{dots_x}: x-values for the \code{dots} dataset
+#'  \item \strong{dino_y}: y-values for \code{dinosaur} dataset!
+#'  \item \strong{dots_x}: x-values for the \code{dots} dataset
 #'  \item \strong{dots_y}: y-values for the \code{dots} dataset
 #'  \item \strong{h_lines_x}: x-values for the \code{h_lines} dataset
 #'  \item \strong{h_lines_y}: y-values for the \code{h_lines} dataset
@@ -54,4 +54,5 @@
 #'  \item \strong{wide_lines_y}: y-values for the \code{wide_lines} dataset
 #'  \item \strong{x_shape_x}: x-values for the \code{x_shape} dataset
 #'  \item \strong{x_shape_y}: y-values for the \code{x_shape} dataset
+#'  }
 "datasaurus_dozen_wide"

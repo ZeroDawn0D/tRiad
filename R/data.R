43111a5ff5de8ca39d9943e287ed7e4bf3cf688d
Umang <umang26112003@gmail.com>
2022-06-18 11:00:28 +0530

    added data.R

---
+++ R/data.R
@@ -0,0 +1,58 @@
+#' Datasaurus Dozen (wide) data
+#'
+#' A dataset demonstrating the utility of visualization.
+#' These 12 datasets are equal in standard measures: mean,
+#' standard deviation, and Pearson's correlation.
+#' @name datasaurus_dozen_wide
+#' @format A data frame with 142 rows and 26 variables:
+#'* **away_x**: x-values for the `away` dataset
+#'* **away_y**: y-values for the `away` dataset
+#'* **bullseye_x**: x-values for the `bullseye` dataset
+#'* **bullseye_y**: y-values for the `bullseye` dataset
+#'* **circle_x**: x-values for the `circle` dataset
+#'* **circle_y**: y-values for the `circle` dataset
+#'* **dino_x**: x-values for `dinosaur` dataset!
+#'* **dino_y**: y-values for `dinosaur` dataset!
+#'* **dots_x**: x-values for the `dots` dataset
+#'* **dots_y**: y-values for the `dots` dataset
+#'* **h_lines_x**: x-values for the `h_lines` dataset
+#'* **h_lines_y**: y-values for the `h_lines` dataset
+#'* **high_lines_x**: x-values for the `high_lines` dataset
+#'* **high_lines_y**: y-values for the `high_lines` dataset
+#'* **slant_down_x**: x-values for the `slant_down` dataset
+#'* **slant_down_y**: y-values for the `slant_down` dataset
+#'* **slant_up_x**: x-values for the `slant_up` dataset
+#'* **slant_up_y**: y-values for the `slant_up` dataset
+#'* **star_x**: x-values for the `star` dataset
+#'* **star_y**: y-values for the `star` dataset
+#'* **v_lines_x**: x-values for the `v_lines` dataset
+#'* **v_lines_y**: y-values for the `v_lines` dataset
+#'* **wide_lines_x**: x-values for the `wide_lines` dataset
+#'* **wide_lines_y**: y-values for the `wide_lines` dataset
+#'* **x_shape_x**: x-values for the `x_shape` dataset
+#'* **x_shape_y**: y-values for the `x_shape` dataset
+#' @references Matejka, J., & Fitzmaurice, G. (2017).
+#' Same Stats, Different Graphs: Generating Datasets with
+#' Varied Appearance and Identical Statistics through Simulated
+#' Annealing. _CHI 2017 Conference proceedings: ACM SIGCHI
+#' Conference on Human Factors in Computing Systems._
+#' Retrieved from [https://www.autodeskresearch.com/publications/samestats](https://www.autodeskresearch.com/publications/samestats). #nolint
+"datasaurus_dozen_wide"
+
+#' Datasaurus Dozen data
+#'
+#' A dataset demonstrating the utility of visualization.
+#' These 12 datasets are equal in standard measures: mean,
+#' standard deviation, and Pearson's correlation.
+#' @name datasaurus_dozen
+#' @format A data frame with 1846 rows and 3 variables:
+#'* **dataset**: indicates which dataset the data are from
+#'* **x**: x-values
+#'* **y**: y-values
+#' @references Matejka, J., & Fitzmaurice, G. (2017).
+#' Same Stats, Different Graphs: Generating Datasets with
+#' Varied Appearance and Identical Statistics through Simulated
+#' Annealing. _CHI 2017 Conference proceedings: ACM SIGCHI
+#' Conference on Human Factors in Computing Systems._
+#' Retrieved from [https://www.autodeskresearch.com/publications/samestats](https://www.autodeskresearch.com/publications/samestats). #nolint
+"datasaurus_dozen"
